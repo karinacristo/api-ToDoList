c3cb71d9e4f604a448ca57d233377318735c3ce5
Karina Cristo <karinacristonegocios@gmail.com>
2022-02-23 14:46:25 -0300

    alterações de nomes na index

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
-// Importando o framework
 import express from "express";
 
-// importanto os controllers
 import usuarioController from "./controllers/usuario-controller.js";
 import tarefaController from "./controllers/tarefa-controller.js";
 
-// Instanciando/criando servidor
 const app = express()
-// Escolhendo a porta
-const port = 3000
+const porta = 3000
 
 // chamando os controllers
 usuarioController(app)
 tarefaController(app)
 
-// Abrindo o servidor na porta escolhida
-app.listen(port, ()=>{
-    console.log(`Servidor aberto na http://localhost:${port}`)
+app.listen(porta, ()=>{
+    console.log(`Servidor aberto na http://localhost:${porta}`)
 })
